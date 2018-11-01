bdbae4b5945ca861e8bddeebad13406b302c1fdb
Navdeep Singh <navdeep.er@gmail.com>
2018-11-01 11:45:57 +0530

    Started with basic create server and haldling requests

--- index.js
+++ index.js
@@ -3,25 +3,87 @@
 // Dependancies
 const http =  require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // Create http server
 const server = http.createServer((req, res) => {
    
   // Parse the url
   const parsedUrl  = url.parse(req.url, true);
+
+  // Get query string object
+  const queryStringObject = parsedUrl.query;
   
   // Get the path
-  let path = parsedUrl.pathname;
-  path = path.replace(/^\/+|\/$/g, '');
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/$/g, '');
 
-  // Send the response
-  res.end('Hello World\n');
+  // Get method
+  const method = req.method;
 
-  // Log the request/response
-  console.log('Request received on path ' + path);
+  // Get Headers
+  const headers = req.headers;
+
+  // Get Payload if any
+  let buffer = '';
+  const decoder = new StringDecoder('utf-8');
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // Construct data
+    const data = {
+      'trimmedPath' : trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': buffer
+    }
+
+    // Choose Handler
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;    
+
+    chosenHandler((statusCode, payload) => {
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      const payloadString = JSON.stringify(payload);
+
+      res.writeHead(statusCode, {
+        'Content-Type' : 'application/json'
+      });
+      res.end(payloadString);
+
+      console.log('Returning the response: ', statusCode, payload);
+    });
+
+
+  })
+
 }); 
 
 // Listen to port
 server.listen(3000, () => {
   console.log('Server is listening up at port 3000');  
 });
+
+// Define Hanlders
+const handlers = {};
+
+// Define Hello Handler
+handlers.hello = (callback) => {
+  callback(null, {'message' : '########Welcome to NodeJS Shakalaka Boom Boom########'})
+}
+
+// Define NotFound Handler
+handlers.notFound = (callback) => {
+  callback(404);
+}
+
+// Define Router
+const router = {
+  'hello' : handlers.hello
+};
+
