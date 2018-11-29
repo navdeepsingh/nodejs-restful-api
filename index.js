c04186c7524fb2f4f6afb2802c832b06621e4d98
Navdeep Singh <navdeep.er@gmail.com>
2018-11-29 17:10:52 +0530

    Working on workers file

--- index.js
+++ index.js
@@ -1,89 +1,24 @@
-// Main API File
+/*
+*
+* Main API File
+*
+*/
 
 // Dependancies
-const http =  require('http');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./lib/config');
-const handlers =  require('./lib/handlers');
-const _data = require('./lib/data');
-const helpers = require('./lib/helpers');
+const server = require('./lib/server');
+//const server = require('./lib/server');
 
-helpers.sendTwilioSms('9872604132', 'hello!', (err)=> {
-  console.log(err);  
-});
+// Declare the app
+const app = {};
 
-// Create http server
-const server = http.createServer((req, res) => {
-
-  // Parse the url
-  const parsedUrl  = url.parse(req.url, true);
-
-  // Get query string object
-  const queryStringObject = parsedUrl.query;
-
-  // Get the path
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/$/g, '');
-
-  // Get method
-  const method = req.method.toLowerCase();
-
-  // Get Headers
-  const headers = req.headers;
-
-  // Get Payload if any
-  let buffer = '';
-  const decoder = new StringDecoder('utf-8');
-  req.on('data', (data) => {
-    buffer += decoder.write(data);
-  });
-  req.on('end', () => {
-    buffer += decoder.end();
-
-    // Construct data
-    const data = {
-      'trimmedPath' : trimmedPath,
-      'queryStringObject': queryStringObject,
-      'method': method,
-      'headers': headers,
-      'payload': helpers.parseJsonToObject(buffer)
-    }
-
-    // Choose Handler
-    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-    chosenHandler(data, (statusCode, payload) => {
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-      payload = typeof(payload) == 'object' ? payload : {};
-
-      const payloadString = JSON.stringify(payload);
-
-      res.writeHead(statusCode, {
-        'Content-Type' : 'application/json'
-      });
-      res.end(payloadString);
-
-      console.log('Returning the response: ', statusCode, payload, method, queryStringObject);
-    });
-
-
-  })
-
-});
-
-// Listen to port
-server.listen(config.port, () => {
-  console.log(`Server is listening up at port ${config.port} and running ${config.envName}`);
-});
-
-
-// Define Router
-const router = {
-  'hello' : handlers.hello,
-  'ping' : handlers.ping,
-  'test': handlers.test,
-  'users' : handlers.users,
-  'tokens' : handlers.tokens,
-  'checks' : handlers.checks
+// Init function
+app.init = () => {
+  // Start the server
+  server.init();
 };
+
+// Execute the function
+app.init();
+
+// Export the app
+//module.exports = app;
